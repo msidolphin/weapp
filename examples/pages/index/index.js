de60a8276a84b3fce2c651d925a75eddca4c937f
msidolphin <OoMass-EffectoO@hotmail.com>
2019-06-02 21:11:59 +0800

    增加滚动选择器和上拉抽屉组件

--- examples/pages/index/index.js
+++ examples/pages/index/index.js
@@ -5,4 +5,119 @@ Page({
             imageUrl: 'https://file.iviewui.com/iview-weapp-logo.png'
         };
     },
+    data: {
+      visible: true,
+      value: ['440000', '440400', '440403'],
+      data: [
+        {
+          id: 1,
+          label: '分类1',
+          children: [
+            {
+              id: 11,
+              label: '分类11',
+              children: [
+                {
+                  id: 111,
+                  label: '分类111'
+                },
+                {
+                  id: 112,
+                  label: '分类112'
+                },
+                {
+                  id: 113,
+                  label: '分类113'
+                },
+                {
+                  id: 114,
+                  label: '分类114'
+                }
+              ]
+            },
+            {
+              id: 12,
+              label: '分类12',
+              children: [
+                {
+                  id: 121,
+                  label: '分类121'
+                },
+                {
+                  id: 122,
+                  label: '分类122'
+                },
+                {
+                  id: 123,
+                  label: '分类123'
+                },
+                {
+                  id: 124,
+                  label: '分类124'
+                }
+              ]
+            },
+            {
+              id: 13,
+              label: '分类13',
+              children: [
+                {
+                  id: 131,
+                  label: '分类131'
+                },
+                {
+                  id: 132,
+                  label: '分类132'
+                },
+                {
+                  id: 133,
+                  label: '分类133'
+                },
+                {
+                  id: 134,
+                  label: '分类134'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          id: 2,
+          label: '分类2',
+          children: [{
+            id: 21,
+            label: '分类21',
+            children: [
+              {
+                id: 211,
+                label: '分类211'
+              },
+              {
+                id: 212,
+                label: '分类212'
+              },
+              {
+                id: 213,
+                label: '分类213'
+              }
+            ]
+          }]
+        }
+      ]
+    },
+  onTap () {
+    this.setData({
+      visible: true
+    })
+  },
+  onChange (e) {
+    this.setData({
+      value: e.detail.value
+    })
+  },
+  onClose () {
+    this.setData({
+      visible: false
+    })
+  }
 });
---
+++ examples/pages/picker/index.js
@@ -0,0 +1,138 @@
+Page({
+  data: {
+    visible1: false,
+    visible2: false,
+    text1: [],
+    text2: [],
+    value1: [2, 21, 212],
+    value2: ['440000', '440400', '440403'],
+    data: [
+      {
+        id: 1,
+        label: '分类1',
+        children: [
+          {
+            id: 11,
+            label: '分类11',
+            children: [
+              {
+                id: 111,
+                label: '分类111'
+              },
+              {
+                id: 112,
+                label: '分类112'
+              },
+              {
+                id: 113,
+                label: '分类113'
+              },
+              {
+                id: 114,
+                label: '分类114'
+              }
+            ]
+          },
+          {
+            id: 12,
+            label: '分类12',
+            children: [
+              {
+                id: 121,
+                label: '分类121'
+              },
+              {
+                id: 122,
+                label: '分类122'
+              },
+              {
+                id: 123,
+                label: '分类123'
+              },
+              {
+                id: 124,
+                label: '分类124'
+              }
+            ]
+          },
+          {
+            id: 13,
+            label: '分类13',
+            children: [
+              {
+                id: 131,
+                label: '分类131'
+              },
+              {
+                id: 132,
+                label: '分类132'
+              },
+              {
+                id: 133,
+                label: '分类133'
+              },
+              {
+                id: 134,
+                label: '分类134'
+              }
+            ]
+          }
+        ]
+      },
+      {
+        id: 2,
+        label: '分类2',
+        children: [{
+          id: 21,
+          label: '分类21',
+          children: [
+            {
+              id: 211,
+              label: '分类211'
+            },
+            {
+              id: 212,
+              label: '分类212'
+            },
+            {
+              id: 213,
+              label: '分类213'
+            }
+          ]
+        }]
+      }
+    ]
+  },
+  onTap1() {
+    this.setData({
+      visible1: true
+    })
+  },
+  onTap2() {
+    this.setData({
+      visible2: true
+    })
+  },
+  onChange1(e) {
+    this.setData({
+      value1: e.detail.value,
+      text1: e.detail.text
+    })
+  },
+  onChange2(e) {
+    this.setData({
+      value2: e.detail.value,
+      text2: e.detail.text
+    })
+  },
+  onClose1() {
+    this.setData({
+      visible1: false
+    })
+  },
+  onClose2() {
+    this.setData({
+      visible2: false
+    })
+  }
+});
