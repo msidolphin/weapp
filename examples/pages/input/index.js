17745a8732c1dc4d84fb5113c6f85d96f94e95b8
msidolphin <OoMass-EffectoO@hotmail.com>
2019-06-06 13:48:56 +0800

    输入框组件支持自定义内容，新增右侧箭头属性，完善输入框demo

--- examples/pages/input/index.js
+++ examples/pages/input/index.js
@@ -1,14 +1,21 @@
 const { $Toast } = require('../../dist/base/index');
 Page({
-    data: {
-        value1: '',
-        value2: '',
-        value3: '',
-        value4: '输入框已禁用',
-        value5: '',
-        value6: '',
-        value7: ''
-    },
+  data: {
+    value1: '',
+    value2: '',
+    value3: '',
+    value4: '输入框已禁用',
+    value5: '',
+    value6: '',
+    value7: '',
+    gender: [
+      { name: 'male', value: '男' },
+      { name: 'female', value: '女'}
+    ],
+    visible: false,
+    text: [],
+    region: ['440000', '440400', '440403']
+  },
   handleIconTap () {
     $Toast({
       content: '你点击了图标'
@@ -17,6 +24,22 @@ Page({
   onChange (e) {
     this.setData({
       value1: e.detail.detail.value
+    })
+  },
+  openPicker () {
+    this.setData({
+      visible: true
+    })
+  },
+  onPickerClose () {
+    this.setData({
+      visible: false
+    })
+  },
+  onPickerChange (e) {
+    this.setData({
+      region: e.detail.value,
+      text: e.detail.text.join('/')
     })
   }
 });
