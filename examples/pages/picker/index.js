de1d08fcf6ee2bf70503213b05d9cda11c73b5ed
msidolphin <OoMass-EffectoO@hotmail.com>
2019-06-28 13:20:08 +0800

    完成日期滚动选择组件

--- examples/pages/picker/index.js
+++ examples/pages/picker/index.js
@@ -2,13 +2,22 @@ Page({
   data: {
     visible1: false,
     visible2: false,
-    visible3: true,
+    visible3: false,
+    visible4: false,
+    visible5: false,
+    visible6: false,
+    visible7: false,
+    visible8: false,
     text1: [],
     text2: [],
     value1: [2, 21, 212],
     value2: ['440000', '440400', '440403'],
-    value3: '',
-    data: []
+    date1: '',
+    date2: '',
+    date3: '',
+    date4: '',
+    date5: '',
+    date6: ''
   },
   onLoad () {
     wx.showLoading({
@@ -115,7 +124,7 @@ Page({
       wx.hideLoading()
     }, 1000)
   },
-  onTap1() {
+  onTap1 () {
     this.setData({
       visible1: true
     })
@@ -130,24 +139,79 @@ Page({
       visible3: true
     })
   },
-  onChange1(e) {
+  onTap4 () {
+    this.setData({
+      visible4: true
+    })
+  },
+  onTap5 () {
+    this.setData({
+      visible5: true
+    })
+  },
+  onTap6 () {
+    this.setData({
+      visible6: true
+    })
+  },
+  onTap7 () {
+    this.setData({
+      visible7: true
+    })
+  },
+  onTap8 () {
+    this.setData({
+      visible8: true
+    })
+  },
+  onChange1 (e) {
     this.setData({
       value1: e.detail.value,
       text1: e.detail.text
     })
   },
-  onChange2(e) {
+  onChange2 (e) {
     this.setData({
       value2: e.detail.value,
       text2: e.detail.text
     })
   },
-  onClose1() {
+  onChange3 (e) {
+    this.setData({
+      date1: e.detail.text
+    })
+  },
+  onChange4 (e) {
+    this.setData({
+      date2: e.detail.text
+    })
+  },
+  onChange5 (e) {
+    this.setData({
+      date3: e.detail.text
+    })
+  },
+  onChange6 (e) {
+    this.setData({
+      date4: e.detail.text
+    })
+  },
+  onChange7 (e) {
+    this.setData({
+      date5: e.detail.text
+    })
+  },
+  onChange8 (e) {
+    this.setData({
+      date6: e.detail.text
+    })
+  },
+  onClose1 () {
     this.setData({
       visible1: false
     })
   },
-  onClose2() {
+  onClose2 () {
     this.setData({
       visible2: false
     })
@@ -155,6 +219,31 @@ Page({
   onClose3 () {
     this.setData({
       visible3: false
+    })
+  },
+  onClose4 () {
+    this.setData({
+      visible4: false
+    })
+  },
+  onClose5 () {
+    this.setData({
+      visible5: false
+    })
+  },
+  onClose6 () {
+    this.setData({
+      visible6: false
+    })
+  },
+  onClose7 () {
+    this.setData({
+      visible7: false
+    })
+  },
+  onClose8 () {
+    this.setData({
+      visible8: false
     })
   }
 });
