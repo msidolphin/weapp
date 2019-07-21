86a92ddfb43618cbd350fe22173ce895e0752a76
msidolphin <OoMass-EffectoO@hotmail.com>
2019-07-21 21:17:27 +0800

    添加文档

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    title: 'Weapp',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+            { text: 'Github', link: 'https://github.com/msidolphin/weapp' }
+        ],
+        sidebar: [
+            '/guide/',
+            {
+                title: '组件',
+                collapsable: false,
+                children: [
+                    {
+                        title: '基础',
+                        collapsable: false,
+                        children: [
+                            '/components/basic/color',
+                            '/components/basic/button'
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+}
